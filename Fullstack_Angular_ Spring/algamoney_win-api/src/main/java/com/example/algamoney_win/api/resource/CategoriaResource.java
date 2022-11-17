0395ffff4890c2d8100060871540392d9de1db69
Fabiano Ventura Guimarães <fabianovguimaraes@hotmail.com>
2022-11-17 10:19:51 -0300

    3.6._Cadastrando_nova_categoria_com_POST
    Criando uma nova categoria com o post

--- Fullstack_Angular_ Spring/algamoney_win-api/src/main/java/com/example/algamoney_win/api/resource/CategoriaResource.java
+++ Fullstack_Angular_ Spring/algamoney_win-api/src/main/java/com/example/algamoney_win/api/resource/CategoriaResource.java
@@ -3,9 +3,13 @@ package com.example.algamoney_win.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney_win.api.model.Categoria;
@@ -24,5 +28,14 @@ public class CategoriaResource {
     public List<Categoria> listar() {
         return categoriaRepository.findAll(); // Voltando para metadado original 
     }
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) //  criacção de uma categoria nova : em pagina 201 , de criação. 
+	public void criar(@RequestBody Categoria categoria) {
+		categoriaRepository.save(categoria);
+		
+		
+	}
 	
 }
