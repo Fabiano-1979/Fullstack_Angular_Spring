577df3907f8aa75d28392a65054d887272355c9e
Fabiano Ventura Guimarães <fabianovguimaraes@hotmail.com>
2022-11-17 09:07:52 -0300

    Coleção_vazia,_o_que_retornar
    one

--- Fullstack_Angular_ Spring/algamoney_win-api/src/main/java/com/example/algamoney_win/api/resource/CategoriaResource.java
+++ Fullstack_Angular_ Spring/algamoney_win-api/src/main/java/com/example/algamoney_win/api/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.example.algamoney_win.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,10 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar (){
-		return categoriaRepository.findAll();
+	public ResponseEntity<?> listar (){         // lista de resposta para reposta vazia , em caso de de erro 404 ou 200 em pagina.
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) :  ResponseEntity.notFound().build(); 
+		// Aplicamos a regra que se a a lista NÃO TIVER VAZIA , SE APLICA A REGRA, se não , aplica se a regra do NOT FOUND --> 400 em status 
+		
 	}
 }
